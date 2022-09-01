94d5f7677079a4b61e1c9d95dd7999c2bdc89ed1
omarmustawi <100845779+omarmustawi@users.noreply.github.com>
2022-09-01 12:53:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+let title = document.querySelector(".title") ;
+let ul = document.querySelector("ul") ;
+let reload = document.querySelector(".reload") ;
+
+window.onload = function(){
+    if( window.navigator.online  ){
+        onLine() ;
+    }else{
+        offLine() ;
+    }
+}
+
+
+window.addEventListener("online" , function(){
+    onLine() ;
+} ) ;
+
+
+window.addEventListener("offline" , function(){
+    offLine() ;
+} ) ;
+
+
+reload.onclick = function(){
+    window.location.reload() ;
+} 
+
+
+function onLine(){
+    title.innerHTML = 'Online Now' ;
+    title.style.color = 'green' ;
+    ul.classList.add('hide') ;
+    reload.classList.add('hide') ;
+}
+
+function offLine(){
+    title.innerHTML = 'Offline Now' ;
+    title.style.color = 'red' ;
+    ul.classList.remove('hide') ;
+    reload.classList.remove('hide') ;
+}
+
+
